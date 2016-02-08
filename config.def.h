275583c0de534c42ba20a7e13235d933915ec26e
mweaver <mweaver@clearwateranalytics.com>
2016-02-08 14:40:10 -0700

    Increased number of tags from 5 to 8

--- config.def.h
+++ config.def.h
@@ -56,7 +56,7 @@ static Color colors[] = {
 /* curses attributes for not selected tags which with urgent windows */
 #define TAG_URGENT (COLOR(BLUE) | A_NORMAL | A_BLINK)
 
-const char tags[][8] = { "1", "2", "3", "4", "5" };
+const char tags[][8] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 #include "tile.c"
 #include "grid.c"
